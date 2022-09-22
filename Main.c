c27fb412ea5858a015a32dd20f6bf8737a3d8f06
mrtsjoao <joao.v.alcantaraa@gmail.com>
2022-09-22 07:50:33 -0300

    teste github

---
+++ Main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    char estudante, listaAlunos;
+    int horarioVan, vagas12, vagas15, vagas17;
+    vagas12 = 17
+    vagas15 = 17
+    vagas17 = 17
+    printf("Digite o nome do aluno: \n");
+    scanf("%c", &estudante);
+    printf("Qual van esse aluno vai? \n");
+    scanf("%d", &horarioVan);
+
+  switch (horarioVan){
+    case 12:
+        listaAlunos = listaAlunos + estudante;
+        vagas12 -- ;
+        break;
+    case 15:
+        listaAlunos;
+  }
+    return 0;
+}
