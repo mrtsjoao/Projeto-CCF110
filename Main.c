611de6a149b103a73aa49e4e9e52913e7e77192e
mrtsjoao <joao.v.alcantaraa@gmail.com>
2022-10-04 13:37:49 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,25 +2,56 @@
 #include <stdlib.h>
 
 
-int main()
-{
-    char estudante, listaAlunos;
-    int horarioVan, vagas12, vagas15, vagas17;
-    vagas12 = 17
-    vagas15 = 17
-    vagas17 = 17
-    printf("Digite o nome do aluno: \n");
-    scanf("%c", &estudante);
-    printf("Qual van esse aluno vai? \n");
-    scanf("%d", &horarioVan);
+int main(){
+  int horario, nVans[3], nVagas[3], cont;
 
-  switch (horarioVan){
-    case 12:
-        listaAlunos = listaAlunos + estudante;
-        vagas12 -- ;
-        break;
-    case 15:
-        listaAlunos;
+  //DEFINIMOS QUE CADA VAN POSSUI 15 VAGAS
+  for (int i = 0; i < 3; i++)
+  {
+    printf("Digite a quantidade de vans: \n");
+    scanf("%d", &nVans[i]);
+    nVagas[i]  = nVans[i] * 15;
+    cont += nVagas[i];
   }
+  for (int i = 0; i < cont; i++)
+  {
+    printf("Digite o horário de saída \n");
+    printf("Horários disponíveis: 12, 15 e 17 \n");
+    scanf("%d",&horario);
+    switch (horario){
+    case 12:
+      if(nVagas[0] <1 ){
+        printf("Vagas esgotadas!");
+      }
+      else{
+        nVagas[0]--;
+        printf("Restam %d vagas para a van de 12 horas\n", nVagas[0]);
+      }
+      break;
+    case 15:
+      if(nVagas[1] <1 ){
+        printf("Vagas esgotadas!");
+      }
+      else{
+        nVagas[1]--;
+        printf("Restam %d vagas para a van de 15 horas\n", nVagas[1]);
+      }
+      break;
+    case 17:
+      if(nVagas[2] <1 ){
+       printf("Vagas esgotadas!");
+      }
+      else{
+        nVagas[2]--;
+        printf("Restam %d vagas para a van de 17 horas\n", nVagas[2]);
+      }
+      break;
+    default:
+      printf("Horário indisponivel\n");
+      break;
+    } 
+  } 
+
     return 0;
+
 }
