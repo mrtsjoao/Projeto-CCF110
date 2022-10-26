c9e617b5cfc4d80f081ae4d9927d0c0ed3bef71b
mrtsjoao <joao.v.alcantaraa@gmail.com>
2022-10-26 18:40:04 -0300

    26/10

--- Main.c
+++ Main.c
@@ -3,18 +3,25 @@
 
 
 int main(){
-  int horario, nVans[3], nVagas[3], cont;
+  int horario, nVans[3], nVagas[3], cont = 0, nVagasVan = 15;
 
-  //DEFINIMOS QUE CADA VAN POSSUI 15 VAGAS
+  /*horario 0 = 12 horas
+    horario 1 = 15 horas
+    horario 1 = 17 horas*/
   for (int i = 0; i < 3; i++)
   {
-    printf("Digite a quantidade de vans: \n");
+    printf("Digite a quantidade de vans no horario (%d): \n", (i));
     scanf("%d", &nVans[i]);
-    nVagas[i]  = nVans[i] * 15;
+    nVagas[i]  = nVans[i] * nVagasVan;
     cont += nVagas[i];
   }
+
+  int IdAluno[cont], OrdemEntrega[cont][3];
+
   for (int i = 0; i < cont; i++)
   {
+    printf("Digite o ID do aluno: \n");
+    scanf("%d", &IdAluno[i]);
     printf("Digite o horário de saída \n");
     printf("Horários disponíveis: 12, 15 e 17 \n");
     scanf("%d",&horario);
@@ -25,6 +32,7 @@ int main(){
       }
       else{
         nVagas[0]--;
+        OrdemEntrega[cont][0] = IdAluno[i];
         printf("Restam %d vagas para a van de 12 horas\n", nVagas[0]);
       }
       break;
@@ -34,6 +42,7 @@ int main(){
       }
       else{
         nVagas[1]--;
+        OrdemEntrega[cont][1] = IdAluno[i];
         printf("Restam %d vagas para a van de 15 horas\n", nVagas[1]);
       }
       break;
@@ -43,15 +52,15 @@ int main(){
       }
       else{
         nVagas[2]--;
+        OrdemEntrega[cont][2] = IdAluno[i];
         printf("Restam %d vagas para a van de 17 horas\n", nVagas[2]);
       }
       break;
     default:
       printf("Horário indisponivel\n");
       break;
-    } 
+    }
   } 
-
     return 0;
 
 }
