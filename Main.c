0005cab9b7a93a022097906d698c86137c79c49b
mrtsjoao <joao.v.alcantaraa@gmail.com>
2022-10-26 18:41:42 -0300

    Código novo

--- Main.c
+++ Main.c
@@ -3,64 +3,88 @@
 
 
 int main(){
-  int horario, nVans[3], nVagas[3], cont = 0, nVagasVan = 15;
+  int nVans[3], nVagas[3], cont = 0, nVagasVan;
+  int nAlunos;
+  
 
-  /*horario 0 = 12 horas
-    horario 1 = 15 horas
-    horario 1 = 17 horas*/
-  for (int i = 0; i < 3; i++)
+  system("cls");
+  printf("Bem vindo usuário!\n");
+  printf(" Digite a quantidade de alunos que vão embora: \n");
+  scanf("%d", &nAlunos);
+  printf("Digite a quantidade de vagas por van: ");
+  scanf("%d", &nVagasVan);
+
+
+  //CALCULO DOS DESENHO DOS ASSENTOS DISPONIVEIS
+  int LinhaAssentos, ColunaAssentos = 3;
+  LinhaAssentos = nAlunos / ColunaAssentos;
+  if (nAlunos % ColunaAssentos != 0)
   {
-    printf("Digite a quantidade de vans no horario (%d): \n", (i));
-    scanf("%d", &nVans[i]);
-    nVagas[i]  = nVans[i] * nVagasVan;
-    cont += nVagas[i];
+    LinhaAssentos++;
   }
 
-  int IdAluno[cont], OrdemEntrega[cont][3];
+  int Assentos[LinhaAssentos][ColunaAssentos];
+  int countVans[2];
+  countVans[0] = 0;
+  countVans[1] = 1;
 
-  for (int i = 0; i < cont; i++)
+  for(int i = 0; i< LinhaAssentos; i++)
   {
-    printf("Digite o ID do aluno: \n");
-    scanf("%d", &IdAluno[i]);
-    printf("Digite o horário de saída \n");
-    printf("Horários disponíveis: 12, 15 e 17 \n");
-    scanf("%d",&horario);
-    switch (horario){
-    case 12:
-      if(nVagas[0] <1 ){
-        printf("Vagas esgotadas!");
-      }
-      else{
-        nVagas[0]--;
-        OrdemEntrega[cont][0] = IdAluno[i];
-        printf("Restam %d vagas para a van de 12 horas\n", nVagas[0]);
-      }
-      break;
-    case 15:
-      if(nVagas[1] <1 ){
-        printf("Vagas esgotadas!");
-      }
-      else{
-        nVagas[1]--;
-        OrdemEntrega[cont][1] = IdAluno[i];
-        printf("Restam %d vagas para a van de 15 horas\n", nVagas[1]);
-      }
-      break;
-    case 17:
-      if(nVagas[2] <1 ){
-       printf("Vagas esgotadas!");
-      }
-      else{
-        nVagas[2]--;
-        OrdemEntrega[cont][2] = IdAluno[i];
-        printf("Restam %d vagas para a van de 17 horas\n", nVagas[2]);
-      }
-      break;
-    default:
-      printf("Horário indisponivel\n");
-      break;
+    for(int j = 0; j < ColunaAssentos; j++)
+    {
+      Assentos[i][j] = 0;
+      countVans[0]++;
     }
-  } 
-    return 0;
+    if(countVans[0] >= nVagasVan)
+    {
+      if(nVagasVan != nAlunos)
+      {
+        countVans[1]++;
+      }
+      countVans[0] = 0;
+    }
+  }
 
+  //LEITURA DOS DADOS DOS ALUNOS
+  
+  for(int I = 0; I < nAlunos; I++)
+  { 
+    system("cls");
+    printf("Serao necessarias ao menos %d van(s).\n", countVans[1]);
+    printf("ASSENTOS DISPONIVEIS SEPARADOS POR VANS\n");
+    for(int i = 0; i< LinhaAssentos; i++)
+    {
+      printf("| ");
+      for(int j = 0; j < ColunaAssentos; j++)
+      {
+        printf("%d ", Assentos[i][j]);
+        countVans[0]++;
+      }
+      printf("| \n");
+      if(countVans[0] >= nVagasVan)
+      {
+        printf("\n");
+        countVans[0] = 0;
+      }
+    }
+    countVans[0] = 0;
+
+
+    int IDs[nAlunos];
+    printf("Digite o ID do alunos: ");
+    scanf("%d", &IDs[I]);
+    printf("\n");
+
+    int posx, posy;
+    printf("Digite o assento que ele irá ocupar: (Linha) (Coluna)\n");
+    scanf("%d %d",&posy, &posx);
+    Assentos[posy][posx] = IDs[I];
+    if (Assentos[posy][posx] != 0)
+    {
+      printf("Lugar já está ocupado!");
+    }
+    
+  }
+  
+  return 0;
 }
