bace07409679ed0a681f798536ece513394e33a2
Cleber Gomes Junior <113527750+CleberGomesJr@users.noreply.github.com>
2022-11-15 14:25:59 -0300

    Joia

--- Main.c
+++ Main.c
@@ -1,26 +1,142 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
+int main(){
+  int nVagasVan =15;
+  int nAlunos;
 
-int main()
-{
-    char estudante, listaAlunos;
-    int horarioVan, vagas12, vagas15, vagas17;
-    vagas12 = 17
-    vagas15 = 17
-    vagas17 = 17
-    printf("Digite o nome do aluno: \n");
-    scanf("%c", &estudante);
-    printf("Qual van esse aluno vai? \n");
-    scanf("%d", &horarioVan);
+  system("cls");
+  printf("Bem vindo usuário!\n");
+  printf("Digite a quantidade de alunos que vão embora: \n");
+  scanf("%d", &nAlunos);
 
-  switch (horarioVan){
-    case 12:
-        listaAlunos = listaAlunos + estudante;
-        vagas12 -- ;
-        break;
-    case 15:
-        listaAlunos;
+  //CALCULO DO DESENHO DOS ASSENTOS DISPONIVEIS
+  int LinhaAssentos, ColunaAssentos = 3;
+  if(nAlunos < nVagasVan)
+  {
+    LinhaAssentos = 5;
   }
-    return 0;
+  else{
+    LinhaAssentos = 5 *ceil(((float)nAlunos / (float)nVagasVan)); // CONSIDERANDO QUE EXISTEM 5 FILEIRAS P/VAN
+  }
+
+  int Assentos[LinhaAssentos][ColunaAssentos];
+  int countVans[2]; // INDICE 0 == CONTADOR POR FILEIRA E INDICE 1 == CONTADOR DE VANS;
+  countVans[0] = 0;
+  countVans[1] = 0;
+  for(int i = 0; i< LinhaAssentos; i++)
+  {
+    for(int j = 0; j < ColunaAssentos; j++)
+    {
+      Assentos[i][j] = 0;
+      countVans[0]++;
+    }
+    if(countVans[0] >= nVagasVan)
+    {
+      if(nVagasVan != nAlunos)
+      {
+        countVans[1]++;
+      }
+      countVans[0] = 0;
+    }
+  }
+
+  //LEITURA DOS DADOS DOS ALUNOS
+    char nomes[nAlunos][30];
+    int IDs[nAlunos];
+for(int I = 0; I < nAlunos; I++)
+  {
+    system("cls");
+    printf("Serao necessarias ao menos %d van(s).\n", countVans[1]);
+    printf("ASSENTOS DISPONIVEIS SEPARADOS POR VANS\n");
+
+    for(int i = 0; i< LinhaAssentos; i++)
+    {
+
+      printf("| ");
+      for(int j = 0; j < ColunaAssentos; j++)
+      {
+        printf("%d ", Assentos[i][j]);
+        countVans[0]++;
+      }
+      printf("| \n");
+      if(countVans[0] >= nVagasVan)
+      {
+        printf("\n");
+        countVans[0] = 0;
+      }
+    }
+    countVans[0] = 0;
+
+    //LISTA DE ALUNOS COM NOMES LIGADOS A MATRICULA POR PONTEIROS
+    fflush(stdin);
+    printf("Digite o %dº nome: ", I+1);
+    scanf("%25[^\n]", nomes[I]);
+    fflush(stdin);
+    printf("Digite o ID do alunos: ");
+    scanf("%d", &IDs[I]);
+
+    int posx, posy, pause;
+    printf("Digite o assento que ele irá ocupar: (Linha) (Coluna)\n");
+    scanf("%d %d",&posy, &posx);
+    if (Assentos[posy][posx] != 0)
+    {
+      printf("Lugar já está ocupado!");
+      printf("O Aluno %d será substituido por %d: Confirme sua ação com (1)", Assentos[posy][posx], IDs[I]);
+      scanf("%d", &pause);
+      Assentos[posy][posx] = IDs[I];
+      I--;
+    }
+    else{
+        Assentos[posy][posx] = IDs[I];
+    }
+  }
+system("cls");
+for(int i = 0; i< LinhaAssentos; i++)
+{
+    printf("| ");
+    for(int j = 0; j < ColunaAssentos; j++)
+    {
+        printf("%d ", Assentos[i][j]);
+        countVans[0]++;
+    }
+    printf("| \n");
+    if(countVans[0] >= nVagasVan)
+    {
+        printf("\n");
+        countVans[0] = 0;
+    }
+
+}
+countVans[0] = 0;
+
+  float Distancia_cidade[3], tempo[3]; // 0 -> BETIM, 1 -> CONTAGEM, 2 -> BH (BARREIRO)
+  float mediaAlunos = nAlunos/countVans[1], *pmedia; // PARA TER UMA MEDIA DE ALUNOS P/VAN
+  pmedia = &mediaAlunos;
+  Distancia_cidade[0] = 25.97 + *pmedia; //CONSIDERANDO QUE A DISTANCIA ENTRE UM ALUNO A OUTRO = 1KM
+  Distancia_cidade[1] = 15.62 + *pmedia;
+  Distancia_cidade[2] = 30.67 + *pmedia;
+  tempo[0] = Distancia_cidade[0] / 30;
+  tempo[1] = Distancia_cidade[1] / 30;
+  tempo[2] = Distancia_cidade[2] / 30;
+
+  int cidade;
+  for(int i = 0; i<countVans[1]; i++)
+  {
+    printf("Digite qual cidade a van vai passar: \n<0> BETIM\n<1> CONTAGEM\n<2> BELO HORIZONTE\n");
+    scanf("%d", &cidade);
+    if(cidade == 0){
+        printf("A distancia media aproximada até Betim e: %.2f KM\nO tempo medio da rota inteira e: %.2f Horas",Distancia_cidade[0], tempo[0]);
+    }
+    if(cidade == 1){
+        printf("A distancia media aproximada até Contagem e: %.2f KM\nO tempo medio da rota inteira e: %.2f Horas",Distancia_cidade[1], tempo[1]);
+    }
+    if(cidade == 2){
+        printf("A distancia media aproximada até Belo Horizonte e: %.2f KM\nO tempo medio da rota inteira e: %.2f Horas",Distancia_cidade[2], tempo[2]);
+    }
+  }
+
+  return 0;
 }
